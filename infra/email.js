d3203e2cab5e52cc67b793b571f97abedbcd693d
MirianFS <mirianfloripa16@gmail.com>
2026-01-27 15:30:21 +0000

    feat: add `email.js` infra module

---
+++ infra/email.js
@@ -0,0 +1,20 @@
+import nodemailer from "nodemailer";
+
+async function send(mailOption) {
+  await transporter.sendMail(mailOption);
+}
+
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_SMTP_HOST,
+  port: process.env.EMAIL_SMTP_PORT,
+  auth: {
+    user: process.env.EMAIL_SMTP_USER,
+    pass: process.env.EMAIL_SMTP_PASSWORD,
+  },
+  secure: process.env.NODE_ENV === "production" ? true : false,
+});
+const email = {
+  send,
+};
+
+export default email;
